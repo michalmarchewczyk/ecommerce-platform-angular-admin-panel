03aba367bed9b31e43f634cd145bb266cd827739
Michał Marchewczyk <dev@marchewczyk.eu>
2022-07-28 00:02:05 +0200

    Setup ESLint and Prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {environment} from '../environments/environment';
-import {ApiModule, Configuration, ConfigurationParameters} from './core/api';
-import {HttpClientModule} from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { ApiModule, Configuration, ConfigurationParameters } from './core/api';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const apiConfigFactory = () => {
-  const params: ConfigurationParameters = {
-    basePath: environment.apiUrl,
-  }
+  const params: ConfigurationParameters = { basePath: environment.apiUrl };
   return new Configuration(params);
-}
+};
+
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -26,6 +23,6 @@ export const apiConfigFactory = () => {
     BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
