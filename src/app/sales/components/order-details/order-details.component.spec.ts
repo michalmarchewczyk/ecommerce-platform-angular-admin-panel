40dc9f2c2ba1095015206674e87ea1cda8ede979
Michał Marchewczyk <dev@marchewczyk.eu>
2022-09-21 00:03:30 +0200

    Fix order details tests

--- src/app/sales/components/order-details/order-details.component.spec.ts
+++ src/app/sales/components/order-details/order-details.component.spec.ts
@@ -1,22 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderDetailsComponent } from './order-details.component';
+import { ReturnAddDialogComponent } from '../return-add-dialog/return-add-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatDialogHarness } from '@angular/material/dialog/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 
 describe('OrderDetailsComponent', () => {
   let component: OrderDetailsComponent;
   let fixture: ComponentFixture<OrderDetailsComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [OrderDetailsComponent],
+      imports: [MatDialogModule, MatButtonHarness],
+      declarations: [OrderDetailsComponent, ReturnAddDialogComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(OrderDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open return add dialog', async () => {
+    const button = await loader.getHarness(
+      MatButtonHarness.with({ text: 'Add return' }),
+    );
+    await button.click();
+
+    const dialog = await loader.getHarness(MatDialogHarness);
+    expect(dialog).toBeTruthy();
   });
 });
