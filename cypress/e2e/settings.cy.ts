be0498781d2f5b08c10c136ae2bdfe4bd27e609f
Michał Marchewczyk <dev@marchewczyk.eu>
2022-10-22 17:05:15 +0200

    Add settings e2e tests

---
+++ cypress/e2e/settings.cy.ts
@@ -0,0 +1,78 @@
+describe('Settings', () => {
+  beforeEach(() => {
+    cy.loginAdmin();
+    cy.visit('/settings');
+    cy.contains('Settings');
+  });
+
+  it('updating settings', () => {
+    cy.get('mat-list-item')
+      .contains('Currency')
+      .parent()
+      .parent()
+      .find('mat-select')
+      .click();
+    cy.get('mat-option').contains('EUR').click({ force: true });
+    cy.get('mat-list-item')
+      .contains('Countries')
+      .parent()
+      .parent()
+      .find('mat-select')
+      .click();
+    cy.get('mat-option').contains('Poland').click({ force: true });
+    cy.get('mat-option').contains('Germany').click({ force: true });
+    cy.get('.cdk-overlay-backdrop').click();
+    cy.reload();
+    cy.get('mat-list-item')
+      .contains('Currency')
+      .parent()
+      .parent()
+      .find('mat-select-trigger')
+      .should('contain.text', 'EUR');
+    cy.get('mat-list-item')
+      .contains('Countries')
+      .parent()
+      .parent()
+      .find('mat-select-trigger')
+      .should('contain.text', 'Poland')
+      .should('contain.text', 'Germany');
+    cy.intercept('PATCH', '/settings/*').as('request');
+    cy.get('mat-list-item')
+      .contains('Countries')
+      .parent()
+      .parent()
+      .find('mat-select')
+      .click();
+    cy.get('mat-option').contains('Poland').click({ force: true });
+    cy.get('mat-option').contains('Germany').click({ force: true });
+    cy.get('.cdk-overlay-backdrop').click();
+    cy.wait('@request');
+  });
+
+  it('adding custom settings', () => {
+    cy.get('.mat-tab-label').contains('Custom').click();
+
+    cy.contains('Type *').parent().parent().click();
+    cy.get('mat-option').contains('Number').click({ force: true });
+    cy.contains('Name *')
+      .parent()
+      .parent()
+      .find('input')
+      .type('Test Cypress Setting');
+    cy.contains('Value *').parent().parent().find('input').type('123');
+    cy.get('button').contains('Add').click();
+    cy.get('mat-list-item').should('contain.text', 'Test Cypress Setting');
+  });
+
+  it('deleting custom settings', () => {
+    cy.get('.mat-tab-label').contains('Custom').click();
+    cy.get('mat-list-item')
+      .contains('Test Cypress Setting')
+      .parent()
+      .parent()
+      .find('button')
+      .click();
+    cy.get('button').contains('Delete').click();
+    cy.get('mat-list-item').should('not.contain.text', 'Test Cypress Setting');
+  });
+});
